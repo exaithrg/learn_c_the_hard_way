65a6b6c74245b6ff4bba1a6e88c5adcf9f4500c1
ysyx_22050043-Geng Haoran <hrgeng@smail.nju.edu.cn>
2023-02-25 20:59:09 +0800

    ex15 OK

---
+++ ex15/modi_ex15.c
@@ -0,0 +1,68 @@
+//***************************************************************
+// Description: 
+// File Name: ex15.c
+// Author: Haoran Geng
+// Email: 
+// Created Time: Sat 25 Feb 2023 08:47:57 PM CST
+// Revision history:
+//***************************************************************
+#include <stdio.h>
+
+int main(int argc, char *argv[])
+{
+    // create two arrays we care about
+    int ages[] = {23, 43, 12, 89, 2};
+    char *names[] = {
+        "Alan", "Frank",
+        "Mary", "John", "Lisa"
+    };
+
+    printf("*names=%p\n",names);
+
+    // safely get the size of ages
+    int count = sizeof(ages) / sizeof(int);
+    int i = 0;
+
+    // first way using indexing
+    for(i = 0; i < count; i++) {
+        printf("%s has %d years alive.\n",
+                names[i], ages[i]);
+    }
+
+    printf("---\n");
+
+    // setup the pointers to the start of the arrays
+    int *cur_age = ages;
+    char **cur_name = names;
+
+    // second way using pointers
+    for(i = 0; i < count; i++) {
+        printf("%s is %d years old.\n",
+                *(cur_name+i), *(cur_age+i));
+        printf("*cur_age=%p\n",(cur_age+i));
+        printf("*cur_name=%p\n",(cur_name+i));
+    }
+
+    printf("---\n");
+
+    // third way, pointers are just arrays
+    for(i = 0; i < count; i++) {
+        printf("%s is %d years old again.\n",
+                cur_name[i], cur_age[i]);
+        printf("*cur_age=%p\n",(cur_age+i));
+        printf("*cur_name=%p\n",(cur_name+i));
+    }
+
+    printf("---\n");
+
+    // fourth way with pointers in a stupid complex way
+    for(cur_name = names, cur_age = ages;
+            (cur_age - ages) < count;
+            cur_name++, cur_age++)
+    {
+        printf("%s lived %d years so far.\n",
+                *cur_name, *cur_age);
+    }
+
+    return 0;
+}
