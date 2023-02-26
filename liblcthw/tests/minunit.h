6e0e67a2e14c258c956745e29849eed9543d3cfe
ysyx_22050043-Geng Haoran <hrgeng@smail.nju.edu.cn>
2023-02-26 21:18:27 +0800

    ex32 make failed

--- liblcthw/tests/minunit.h
+++ liblcthw/tests/minunit.h
@@ -1,11 +1,41 @@
 //***************************************************************
 // Description: 
-// File Name: tests/minunit.h
+// File Name: minunit.h
 // Author: Haoran Geng
 // Email: 
-// Created Time: Sun 26 Feb 2023 08:05:07 PM CST
+// Created Time: Sun 26 Feb 2023 08:53:01 PM CST
 // Revision history:
 //***************************************************************
+#undef NDEBUG
+#ifndef _minunit_h
+#define _minunit_h
+
+#include <stdio.h>
+#include <stdlib.h>
 #include "../src/lcthw/dbg.h"
 
+#define mu_suite_start() char *message = NULL
 
+#define mu_assert(test, message) if (!(test)) { log_err(message); return message; }
+#define mu_run_test(test) debug("\n-----%s", " " #test); \
+    message = test(); tests_run++; if (message) return message;
+
+#define RUN_TESTS(name) int main(int argc, char *argv[]) {\
+    argc = 1; \
+    debug("----- RUNNING: %s", argv[0]);\
+        printf("----\nRUNNING: %s\n", argv[0]);\
+        char *result = name();\
+        if (result != 0) {\
+            printf("FAILED: %s\n", result);\
+        }\
+        else {\
+            printf("ALL TESTS PASSED\n");\
+        }\
+    printf("Tests run: %d\n", tests_run);\
+        exit(result != 0);\
+}
+
+// may cause problems
+int tests_run;
+
+#endif
