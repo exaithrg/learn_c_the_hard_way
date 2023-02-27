fd076becf5e809c6f2907606e8d3c290b08cfaa8
ysyx_22050043-Geng Haoran <hrgeng@smail.nju.edu.cn>
2023-02-27 13:31:05 +0800

    why my 32 always failed??

---
+++ list-ex32/liblcthw/tests/minunit.h
@@ -0,0 +1,42 @@
+//***************************************************************
+// Description: 
+// File Name: minunit.h
+// Author: Haoran Geng
+// Email: 
+// Created Time: Sun 26 Feb 2023 08:53:01 PM CST
+// Revision history:
+//***************************************************************
+#undef NDEBUG
+#ifndef _minunit_h
+#define _minunit_h
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <lcthw/dbg.h>
+// #include "../src/lcthw/dbg.h"
+
+#define mu_suite_start() char *message = NULL
+
+#define mu_assert(test, message) if (!(test)) { log_err(message); return message; }
+#define mu_run_test(test) debug("\n-----%s", " " #test); \
+    message = test(); tests_run++; if (message) return message;
+
+#define RUN_TESTS(name) int main(int argc, char *argv[]) {\
+    argc = 1; \
+    debug("----- RUNNING: %s", argv[0]);\
+        printf("----\nRUNNING: %s\n", argv[0]);\
+        char *result = name();\
+        if (result != 0) {\
+            printf("FAILED: %s\n", result);\
+        }\
+        else {\
+            printf("ALL TESTS PASSED\n");\
+        }\
+    printf("Tests run: %d\n", tests_run);\
+        exit(result != 0);\
+}
+
+// may cause problems
+int tests_run;
+
+#endif
