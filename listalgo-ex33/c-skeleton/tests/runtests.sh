e5c06068f9d5bce1a97e276b794e5b90aa9eec5e
ysyx_22050043-Geng Haoran <hrgeng@smail.nju.edu.cn>
2023-02-27 14:36:19 +0800

    colorful c-skeleton

--- listalgo-ex33/c-skeleton/tests/runtests.sh
+++ listalgo-ex33/c-skeleton/tests/runtests.sh
@@ -6,7 +6,16 @@
 # Created Time: Sun 26 Feb 2023 05:30:48 PM CST
 # Revision history:
 #***************************************************************
-echo "Running unit tests:"
+
+# color the output nicely
+RED='\e[1;31m'
+GREEN='\e[1;32m'
+YELLOW='\e[1;33m'
+BLUE='\e[1;34m'
+PINK='\e[1;35m'
+NC='\e[0m'
+
+echo "${YELLOW}Running unit tests:${NC}"
 
 for i in tests/*_tests
 do
@@ -14,11 +23,12 @@ do
     then
         if $VALGRIND ./$i 2>> tests/tests.log
         then
-            echo $i PASS
+            echo "${GREEN}$i PASS${NC}"
         else
-            echo "ERROR in test $i: here's tests/tests.log"
+            echo "${RED}ERROR in test $i${NC}" 
+            echo "For more info, see tests.log"
             echo "------"
-            tail tests/tests.log
+            #tail tests/tests.log
             exit 1
         fi
     fi
