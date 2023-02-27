4ebdf39e517731975b9e291ac3a7c899adadeda5
ysyx_22050043-Geng Haoran <hrgeng@smail.nju.edu.cn>
2023-02-27 15:17:25 +0800

    ex33 ready to start

--- listalgo-ex33/liblcthw/src/lcthw/list_algos.c
+++ listalgo-ex33/liblcthw/src/lcthw/list_algos.c
@@ -8,6 +8,31 @@
 //***************************************************************
 #include "list_algos.h"
 
+// Example: bubble sort with function pointer
+// int *bubble_sort(int *numbers, int count, compare_cb cmp)
+// {
+//     int temp = 0;
+//     int i = 0;
+//     int j = 0;
+//     int *target = malloc(count * sizeof(int));
+// 
+//     if(!target) die("Memory error.");
+// 
+//     memcpy(target, numbers, count * sizeof(int));
+// 
+//     for(i = 0; i < count; i++) {
+//         for(j = 0; j < count - 1; j++) {
+//             if(cmp(target[j], target[j+1]) > 0) {
+//                 temp = target[j+1];
+//                 target[j+1] = target[j];
+//                 target[j] = temp;
+//             }
+//         }
+//     }
+// 
+//     return target;
+// }
+
 int List_bubble_sort(List *list, comp_ll cmp){
     return 0;
 }
